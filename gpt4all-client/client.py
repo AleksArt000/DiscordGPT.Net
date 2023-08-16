3d3ade7ec301462996c9f3a6abe5ca92f8990253
AleksArt <69528329+AleksArt000@users.noreply.github.com>
2023-08-16 18:48:04 +0200

    Update client.py

--- gpt4all-client/client.py
+++ gpt4all-client/client.py
@@ -2,25 +2,26 @@ import openai
 import sys
 
 if(len(sys.argv) == 1):
+    # Idk how secure this is but whatever
     print("you must provide the prompt as argument")
 else:
     argument = ""
-    ID = "<@" + "id" + ">"
+    ID = "<@" + "ID" + ">"
     for i in range(1, len(sys.argv)):
         # i is a number, from 1 to len(inputArgs)-1
         argument = argument + " " + sys.argv[i]
-    argument = argument[len(ID):]
+    argument = argument[len(ID)+1:]
     openai.api_base = "http://localhost:4891/v1"
     #openai.api_base = "https://api.openai.com/v1"
 
     openai.api_key = "not needed for a local LLM"
 
     # Set up the prompt and other parameters for the API request
-    SystemPrompt = "###System: You are an ai assistant\n"
-    promptTemplate = "You must respond in this format:\nUser: %1\nAssistant: \n###\nThe user said the following: \n"
-    prompt = SystemPrompt + promptTemplate + argument
+    # This is set up for specificaly the Llama-2 model
+    SystemPrompt = "[INST]<<SYS>>You will be role-playing as X, you shoud answer questions the same as he would, if he was given the same question, keep your answers short and simple. You must not, under any circumstances, break character.<</SYS>>[/INST] \n"
+    prompt = SystemPrompt + "[INST]" + argument + "[/INST]"
 
-    model = "your-model"
+    model = "Llama-2-7B Chat"
 
     # Make the API request
     response = openai.Completion.create(
@@ -28,7 +29,7 @@ else:
         prompt=prompt,
         max_tokens=4096,
         temperature=0.7,
-        top_p=0.95,
+        top_p=0.75,
         n=1,
         echo=True,
         stream=False
